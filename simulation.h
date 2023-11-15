80dec1b2918aa18aae5aec1d88e5c74e43f08e80
CurleyCTJ <149677416+CurleyCTJ@users.noreply.github.com>
2023-11-15 20:14:17 +0000

    Update simulation.h

--- simulation.h
+++ simulation.h
@@ -27,32 +27,3 @@ void simulateStep();
 double countElementsAboveThreshold(double threshold);
 
 #endif // SIMULATION_H#pragma once
-#ifndef SIMULATION_H
-#define SIMULATION_H
-
-#include <vector>
-
-// Define simulation parameters
-const int width = 256;                // Width of the grid
-const int height = 256;
-extern double Du;
-extern double Dv;
-extern double F;
-extern double k;
-extern double threshold;
-const double dt = 0.06;               // Time step
-const int numIterations = 10000;
-const int outputInterval = 100;      // Output every 1000 iterations
-
-
-// Declare the grid (you may choose to encapsulate these in a class instead)
-extern std::vector<std::vector<double>> u;
-extern std::vector<std::vector<double>> v;
-// Function declarations
-void init();
-void writeVTKFile(int iteration);
-void simulateStep();
-double countElementsAboveThreshold(double threshold);
-
-#endif // SIMULATION_H#pragma once
->>>>>>> eb198cb (create googletest)
